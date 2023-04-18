9a2157b9280fc8b88ae53d90f88438bfd942c627
Rusith <rusith@mail.com>
2023-04-18 15:29:25 +0200

    added a workflow to run tests

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+fn main() {}
+
+pub fn plus(num1: i32, num2: i32) -> i32 {
+    return num1 + num2 + 1;
 }
+
+#[cfg(test)]
+mod test;
---
+++ api/src/test/main.rs
@@ -0,0 +1,6 @@
+use crate::plus;
+
+#[test]
+fn it_works() {
+    assert_eq!(plus(2, 2), 4)
+}
