a40ab06ecf4527cc9195725d851d2c720befd05d
Rusith <rusith@mail.com>
2023-04-24 13:05:57 +0200

    added Rocket as a Dependency

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,8 +1,11 @@
-fn main() {}
+#[macro_use] extern crate rocket;
 
-pub fn plus(num1: i32, num2: i32) -> i32 {
-    return num1 + num2;
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
 }
 
-#[cfg(test)]
-mod test;
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
+}
--- api/src/test/main.rs
+++
@@ -1,6 +0,0 @@
-use crate::plus;
-
-#[test]
-fn it_works() {
-    assert_eq!(plus(2, 2), 4)
-}
