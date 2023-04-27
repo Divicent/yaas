42cbae8707f71af60a5b7785245c972c89beb6f4
Rusith <rusith@mail.com>
2023-04-27 20:16:28 +0200

    implemented a route guard

--- api/src/app/auth/models.rs
+++ api/src/app/auth/models.rs
@@ -1,27 +1,32 @@
-use rocket::serde::json::serde_json::de;
-use rocket::{Responder, response};
+use jsonwebtoken::errors::Error;
+use rocket::http::Status;
+use rocket::request::{FromRequest, Outcome, Request};
 use rocket::serde::Serialize;
+use rocket::Responder;
 use serde::Deserialize;
+use serde_json;
+
+
+use crate::app::auth::decode_jwt;
 
 #[derive(Responder, Debug)]
 pub enum NetworkResponse {
-  #[response(status = 201)]
-  Created(String),
-  #[response(status = 400)]
-  BadRequest(String),
-  #[response(status = 401)]
-  Unauthorized(String),
-  #[response(status = 404)]
-  NotFound(String),
-  #[response(status = 409)]
-  Conflict(String),
+    #[response(status = 201)]
+    Created(String),
+    #[response(status = 400)]
+    BadRequest(String),
+    #[response(status = 401)]
+    Unauthorized(String),
+    #[response(status = 404)]
+    NotFound(String),
+    #[response(status = 409)]
+    Conflict(String),
 }
-
 
 #[derive(Serialize)]
 pub enum ResponseBody {
-  Message(String),
-  AuthToken(String)
+    Message(String),
+    AuthToken(String),
 }
 
 #[derive(Serialize)]
@@ -30,20 +35,92 @@ pub struct Response {
     pub body: ResponseBody,
 }
 
-
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Claims {
-  pub subject_id: String,
-  pub exp: usize,
+    pub subject_id: String,
+    pub exp: usize,
 }
 
 #[derive(Debug)]
 pub struct JWT {
-  pub claims: Claims
+    pub claims: Claims,
+}
+
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for JWT {
+    type Error = NetworkResponse;
+
+    async fn from_request(req: &'r Request<'_>) -> Outcome<Self, NetworkResponse> {
+        fn is_valid(key: &str) -> Result<Claims, Error> {
+            Ok(decode_jwt(key.to_string())?)
+        }
+
+        match req.headers().get_one("authorization") {
+            None => {
+                let response = Response {
+                    body: ResponseBody::Message(String::from(
+                        "Error validating JWT token - No token provided",
+                    )),
+                };
+
+                Outcome::Failure((
+                    Status::Unauthorized,
+                    NetworkResponse::Unauthorized(serde_json::to_string(&response).unwrap()),
+                ))
+            }
+            Some(key) => match is_valid(key) {
+                Ok(claims) => Outcome::Success(JWT { claims }),
+                Err(err) => match &err.kind() {
+                    jsonwebtoken::errors::ErrorKind::ExpiredSignature => {
+                        let response = Response {
+                            body: ResponseBody::Message(format!(
+                                "Error validating JWT token - Expired Token"
+                            )),
+                        };
+                        Outcome::Failure((
+                            Status::Unauthorized,
+                            NetworkResponse::Unauthorized(
+                                serde_json::to_string(&response).unwrap(),
+                            ),
+                        ))
+                    }
+                    jsonwebtoken::errors::ErrorKind::InvalidToken => {
+                        let response = Response {
+                            body: ResponseBody::Message(format!(
+                                "Error validating JWT token - Invalid Token"
+                            )),
+                        };
+
+                        Outcome::Failure((
+                            Status::Unauthorized,
+                            NetworkResponse::Unauthorized(
+                                serde_json::to_string(&response).unwrap(),
+                            ),
+                        ))
+                    }
+                    _ => {
+                        let response = Response {
+                            body: ResponseBody::Message(format!(
+                                "Error validating JWT token - {}",
+                                err
+                            )),
+                        };
+
+                        Outcome::Failure((
+                            Status::Unauthorized,
+                            NetworkResponse::Unauthorized(
+                                serde_json::to_string(&response).unwrap(),
+                            ),
+                        ))
+                    }
+                },
+            },
+        }
+    }
 }
 
 pub struct User {
-  pub id: String,
-  pub user_name: String,
-  pub password: String,
+    pub id: String,
+    pub user_name: String,
+    pub password: String,
 }
--- api/src/main.rs
+++ api/src/main.rs
@@ -1,5 +1,5 @@
 #[macro_use] extern crate rocket;
-use app::{ Node, get_node_list, auth::models::{User, Response, ResponseBody, NetworkResponse}};
+use app::{ Node, get_node_list, auth::models::{ Response, ResponseBody, NetworkResponse, JWT}};
 use rocket::serde::json::Json;
 use dotenvy::dotenv;
 use domain::auth::login_user;
@@ -21,8 +21,11 @@ fn ping() -> &'static str {
 }
 
 #[get("/nodes")]
-fn node_list() -> Json<Vec<Node>> {
-    Json(get_node_list())
+fn node_list(key: Result<JWT, NetworkResponse>) ->  Result<Json<String>, NetworkResponse >{
+    match key {
+        Ok(key) => return Ok(Json(key.claims.subject_id)),
+        Err(err) =>  return Err(err) 
+    }
 }
 
 #[post("/auth/sign-in", format="json", data="<login_request>")]
