447dd418db1b14e69637ad8d63ea70956ed39a84
Rusith <rusith@mail.com>
2023-05-11 11:31:53 +0200

    added cors config

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,5 +1,6 @@
 #[macro_use]
 extern crate rocket;
+extern crate rocket_cors;
 
 mod models;
 mod repository;
@@ -8,6 +9,12 @@ use app::auth::handlers::{sign_in_handler, sign_up_handler};
 use domain::auth::Auth;
 use dotenvy::dotenv;
 use mongodb::Client;
+use rocket::{
+    fairing::{Fairing, Info, Kind},
+    http::Header,
+    Request, Response,
+};
+type HttpMethod = rocket::http::Method;
 use std::env;
 
 mod domain {
@@ -26,6 +33,9 @@ mod app {
 fn ping() -> &'static str {
     "Pong"
 }
+
+#[options("/<_..>")]
+fn all_options() {}
 
 #[launch]
 async fn rocket() -> _ {
@@ -49,5 +59,31 @@ async fn rocket() -> _ {
 
     rocket::build()
         .manage(auth)
-        .mount("/v1", routes![ping, sign_in_handler, sign_up_handler])
+        .mount(
+            "/v1",
+            routes![ping, sign_in_handler, sign_up_handler, all_options],
+        )
+        .attach(Cors)
+}
+
+pub struct Cors;
+
+#[rocket::async_trait]
+impl Fairing for Cors {
+    fn info(&self) -> Info {
+        Info {
+            name: "Cross-Origin-Resource-Sharing Fairing",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, PATCH, PUT, DELETE, HEAD, OPTIONS, GET",
+        ));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
 }
